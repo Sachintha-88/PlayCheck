be8d1a07e1e0538dcd210bc55d8025b360230ae8
Sachintha Nirmal <sachinthanirmal658@gmail.com>
2025-12-15 02:20:11 +0530

    add games

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,7 +1,17 @@
 import Header from '../Component/Header'
 import GameCard from '../Component/GameCard'
+
+//game images
 import acBrotherhood from '../assets/acbrotherhood.jpg'
+import acRev from '../assets/acrev.jpg'
 import fh5 from '../assets/fh5.jpg'
+import acVlhlla from '../assets/acVlhlla.jpg'
+import acBlackflag from '../assets/acBlackflag.jpg'
+import acSyndicate from '../assets/acSydicate.jpg'
+import farcry3 from '../assets/farcry3.jpg'
+import farcry6 from '../assets/farcry6.jpg'
+
+
 import Footer from '../Component/Footer'
 
 function Home() {
@@ -11,18 +21,12 @@ function Home() {
       <div className="flex flex-wrap mt-8"> 
         <GameCard src={acBrotherhood} name="Assasin's Creed BrotherHood" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
         <GameCard src={fh5} name="Forza Horizon 5" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
-        <GameCard src={acBrotherhood} name="Assasin's Creed BrotherHood" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
-        <GameCard src={fh5} name="Forza Horizon 5" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
-        <GameCard src={acBrotherhood} name="Assasin's Creed BrotherHood" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
-        <GameCard src={fh5} name="Forza Horizon 5" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
-        <GameCard src={acBrotherhood} name="Assasin's Creed BrotherHood" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
-        <GameCard src={fh5} name="Forza Horizon 5" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
-        <GameCard src={acBrotherhood} name="Assasin's Creed BrotherHood" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
-        <GameCard src={fh5} name="Forza Horizon 5" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
-        <GameCard src={acBrotherhood} name="Assasin's Creed BrotherHood" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
-        <GameCard src={fh5} name="Forza Horizon 5" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
-        <GameCard src={acBrotherhood} name="Assasin's Creed BrotherHood" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
-        <GameCard src={fh5} name="Forza Horizon 5" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
+        <GameCard src={acRev} name="Assasin's Creed Revelations" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
+        <GameCard src={acVlhlla} name="Assasin's Creed Valhalla" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
+        <GameCard src={acBlackflag} name="Assasin's Creed Black Flag" genres="Action, Open world, Stealth, Third-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
+        <GameCard src={acSyndicate} name="Assasin's Creed Syndicate" genres="Race, Open world, Drive, Multiplayer" version="v12.0.2" language="Rus/Eng/Multi" size={95}/>
+        <GameCard src={farcry3} name="Far Cry 3" genres="Action, Open world, Stealth, First-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
+        <GameCard src={farcry6} name="Far Cry 6"  genres="Action, Open world, Stealth, First-person" version="v1.0.2" language="Rus/Eng/Multi" size={50}/>
     </div>
     <Footer/>
     </div>
