739c39183231c137bf99be266b4b3a598d61d026
Sachintha Nirmal <sachinthanirmal658@gmail.com>
2026-02-12 10:07:17 +0530

    simple change

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
         <footer className="bg-white shadow-inner px-6 py-10 mt-12 bg-linear-to-br from-blue-200 via-blue-400 to-white-50">
             <div className="flex justify-between">
                 <div>
-                    <h1 className="text-3xl font-extrabold bg-gradient-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">
+                    <h1 className="text-3xl font-extrabold bg-linear-to-r from-blue-400 to-purple-500 text-transparent bg-clip-text">
                         PlayCheck
                     </h1>
                 </div>
